03de46a2d913f654e9d1eae2da9ce512da77b72d
suryakantam1 <suryakantam2204@gmail.com>
2022-08-17 16:53:29 -0500

    lsn 3 Exercise

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,15 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        class Student{
+            int numberOfCredits =1;
+            double GPA =4.0;
+        }
+
+        public class Course{
+            private String topic;
+            private Teacher instructor;
+            private ArrayList<Student> enrolledStudents;
+        }
     }
 }
